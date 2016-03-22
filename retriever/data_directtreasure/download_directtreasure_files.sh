e6ad398bd2224e159fcc87cfafe8ecf21fde44b7
adolfocorreia <adolfocorreia@users.noreply.github.com>
2016-03-22 09:12:32 -0300

    Fail bash scripts on first error

--- retriever/data_directtreasure/download_directtreasure_files.sh
+++ retriever/data_directtreasure/download_directtreasure_files.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+set -e
+
 # Check if argument ($1) is a valid year (4 digits starting with 19 or 20)
 [[ $1 =~ ^(19|20)[0-9]{2}$ ]] && YEAR=$1
 # If argument is not present or is invalid, use current year
