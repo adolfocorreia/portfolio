3aa07b98cca96c71f297efa489bfde582bb6c1b6
Adolfo Correia <adolfogsc@gmail.com>
2020-10-06 15:41:07 -0300

    Check for IPCA files before downloading

--- retriever/data_ipca/ipca_retriever.py
+++ retriever/data_ipca/ipca_retriever.py
@@ -17,7 +17,8 @@ assert re.match(r"^(19|20)[0-9]{2}$", str(YEAR))
 
 
 IPCA_MAIN_URL = "ftp://ftp.ibge.gov.br/Precos_Indices_de_Precos_ao_Consumidor/IPCA/Serie_Historica/ipca_SerieHist.zip"
-IPCA_MAIN_FILE = "ipca_SerieHist.zip"
+IPCA_ZIP_FILE = "ipca_SerieHist.zip"
+IPCA_MAIN_FILE = "ipca_SerieHist.xls"
 
 # http://www.anbima.com.br/pt_br/informar/estatisticas/precos-e-indices/projecao-de-inflacao-gp-m.htm
 # http://www.anbima.com.br/pt_br/informar/tabela-de-indicadores.htm
@@ -49,20 +50,27 @@ MONTHS = {
     "DEZ": 12
 }
 
+TODAY = dt.datetime.combine(dt.date.today(), dt.datetime.min.time())
+
 
 # Download and extract main data file
-urllib.urlretrieve(IPCA_MAIN_URL, IPCA_MAIN_FILE)
+try:
+    modification_time = dt.datetime.fromtimestamp(os.path.getmtime(IPCA_MAIN_FILE))
+except OSError:
+    modification_time = dt.datetime.fromtimestamp(0)
 
-with zipfile.ZipFile(IPCA_MAIN_FILE, 'r') as myzip:
-    assert len(myzip.namelist()) == 1
-    excel_file_name = myzip.namelist()[0]
-    assert excel_file_name[-3:] == "xls"
-    myzip.extract(excel_file_name)
+if modification_time < TODAY:
+    urllib.urlretrieve(IPCA_MAIN_URL, IPCA_ZIP_FILE)
+
+    with zipfile.ZipFile(IPCA_ZIP_FILE, 'r') as myzip:
+        assert len(myzip.namelist()) == 1
+        filename = myzip.namelist()[0]
+        myzip.extract(filename)
+        os.rename(filename, IPCA_MAIN_FILE)
 
 
 # Load data file
-df = pd.read_excel(excel_file_name, index_col=None, names=COL_NAMES, skiprows=5)
-os.remove(excel_file_name)
+df = pd.read_excel(IPCA_MAIN_FILE, index_col=None, names=COL_NAMES, skiprows=5)
 
 
 # Preprocess data
@@ -77,29 +85,35 @@ for i in range(len(df)):
 
 df["Mes"] = df.Mes.apply(lambda x: MONTHS[x])
 df.drop(axis=1, inplace=True,
-        columns=["NumeroIndice","Perc3Meses","Perc6Meses","PercNoAno","Perc12Meses"])
+        columns=["NumeroIndice", "Perc3Meses", "Perc6Meses", "PercNoAno", "Perc12Meses"])
 
 
 # Download and load projection data
-urllib.urlretrieve(IPCA_PROJECTION_URL, IPCA_PROJECTION_FILE)
+try:
+    modification_time = dt.datetime.fromtimestamp(os.path.getmtime(IPCA_PROJECTION_FILE))
+except OSError:
+    modification_time = dt.datetime.fromtimestamp(0)
+
+if modification_time < TODAY:
+    urllib.urlretrieve(IPCA_PROJECTION_URL, IPCA_PROJECTION_FILE)
+
 df_proj = pd.read_excel(IPCA_PROJECTION_FILE, index_col=None)
 
 
 # Check data consistency
-assert df_proj.iloc[12,0] == "IPCA1"
-assert df_proj.iloc[12,1][0:5] == "Proje"  # Example: "Projecao (dez/18)"
-today = dt.date.today()
-year = df_proj.iloc[12,1][-3:-1]
-assert year == today.strftime("%y"), "Unexpected projection year: %s" % year
-month = df_proj.iloc[12,1][-7:-4]
-assert MONTHS[month.upper()] == today.month, "Unexpected projection month: %s" % month
+assert df_proj.iloc[12, 0] == "IPCA1"
+assert df_proj.iloc[12, 1][0:5] == "Proje"  # Example: "Projecao (dez/18)"
+year = df_proj.iloc[12, 1][-3:-1]
+assert year == TODAY.strftime("%y"), "Unexpected projection year: %s" % year
+month = df_proj.iloc[12, 1][-7:-4]
+assert MONTHS[month.upper()] == TODAY.month, "Unexpected projection month: %s" % month
 
 
 # Append present month projection rate
 rows = {
-    "Ano": [today.year],
-    "Mes": [today.month],
-    "PercNoMes": [df_proj.iloc[12,2]]
+    "Ano": [TODAY.year],
+    "Mes": [TODAY.month],
+    "PercNoMes": [df_proj.iloc[12, 2]]
 }
 df = df.append(pd.DataFrame.from_dict(rows))
 
@@ -122,6 +136,6 @@ with open(csv_file_name, 'w') as csv_file:
     csv_file.write("Dia,Taxa\n")
     day = FIRST_DAY
     while day <= LAST_DAY:
-        rate = df.loc[day.year,day.month].TaxaDiaria
+        rate = df.loc[day.year, day.month].TaxaDiaria
         csv_file.write("%s,%s\n" % (day, rate))
         day += DELTA
