fc15f856699a9e41f20ddb912ed1dc746b7700c4
Adolfo Correia <adolfogsc@gmail.com>
2025-10-22 13:10:10 -0300

    Only download missing days when updating curves

--- retriever/data_curves/download_curves.R
+++ retriever/data_curves/download_curves.R
@@ -23,18 +23,34 @@ if (length(argv) > 0) {
 }
 
 
+update_curve_data <- function(file_name, yc_get_function) {
+  if (file.exists(file_name)) {
+    previous_df <- readr::read_csv(file_name, show_col_types = FALSE)
+    first_date <- as.Date(max(previous_df$refdate)) + 1
+  } else {
+    previous_df <- NULL
+    first_date <- make_date(YEAR, 1, 1)
+  }
+  last_date <- min(make_date(YEAR, 12, 31), NOW)
+
+  new_df <- yc_get_function(first_date = first_date, last_date = last_date)
+
+  df <- rbind(previous_df, new_df)
+  df <- dplyr::distinct(df)
+
+  readr::write_csv(df, file_name)
+}
+
+
 library(rb3)
 # options(rb3.cachedir = file.path(here::here(), 'rb3-cache'))
 options(rb3.clear.cache = TRUE)
 options(rb3.silent = TRUE)
 
-# TODO: set first date as last date (+1) in CSV file
-first_date <- make_date(YEAR, 1, 1)
-last_date <- min(make_date(YEAR, 12, 31), NOW)
+print('Downloading DI x Pre curves...')
+pre_file_name <- file.path(here::here(), paste0('yc_di_pre_', YEAR, '.csv'))
+update_curve_data(pre_file_name, yc_mget)
 
-print('Downloading DI x Pre and DI x IPCA curves...')
-df_pre <- yc_mget(first_date = first_date, last_date = last_date)
-df_ipca <- yc_ipca_mget(first_date = first_date, last_date = last_date)
-
-readr::write_csv(df_pre,  file.path(here::here(), paste0('yc_di_pre_', YEAR, '.csv')))
-readr::write_csv(df_ipca, file.path(here::here(), paste0('yc_di_ipca_', YEAR, '.csv')))
+print('Downloading DI x IPCA curves...')
+ipca_file_name <- file.path(here::here(), paste0('yc_di_ipca_', YEAR, '.csv'))
+update_curve_data(ipca_file_name, yc_ipca_mget)
